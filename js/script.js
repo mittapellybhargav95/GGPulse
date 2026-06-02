f29c961e271e4906aee48c30e6c09e709d9a2547
mittapellybhargav95 <mittapellybhargav95@gmail.com>
2026-06-02 13:02:35 +0530

    describe the changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,77 @@
 const searchInput = document.getElementById("searchInput");
+const searchResults = document.getElementById("searchResults");
+
+const games = [
+
+    {
+        name: "Truck Simulator Ultimate",
+        page: "simulator.html#"
+    },
+
+    {
+        name: "Bus Simulator Ultimate",
+        page: "simulator.html"
+    },
+
+    {
+        name: "World Bus Driving Simulator",
+        page: "simulator.html"
+    },
+
+    {
+        name: "truck simulator big rigs",
+        page: "simulator.html"
+    },
+
+    {
+        name: "truckers of europe 3",
+        page: "simulator.html"
+    },
+
+    {
+        name: "truckers brasil online",
+        page: "simulator.html"
+    },
+
+    {
+        name: "bus simulator indonesia",
+        page: "simulator.html"
+    },
+
+    {
+        name: "truck master: india simulator",
+        page: "simulator.html"
+    },
+
+    {
+        name: "truck simulator evo: drive usa",
+        page: "simulator.html"
+    }
+
+];
 
 searchInput.addEventListener("keyup", function() {
 
-    let filter = searchInput.value.toLowerCase();
+    const filter = searchInput.value.toLowerCase();
 
-    let cards = document.querySelectorAll(".card");
+    searchResults.innerHTML = "";
 
-    cards.forEach(function(card) {
+    if(filter.length === 0){
+        return;
+    }
 
-        let text = card.innerText.toLowerCase();
+    games.forEach(function(game){
 
-        if(text.includes(filter)){
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
+        if(game.name.toLowerCase().includes(filter)){
+
+            searchResults.innerHTML += `
+                <div class="search-item">
+                    <a href="${game.page}">
+                        ${game.name}
+                    </a>
+                </div>
+            `;
+
         }
 
     });
